774a4d05f6c232d0f2f3e151125daa7011ace6b0
danielmarino12 <danielvasquezmartinez@gmail.com>
2017-05-07 17:47:01 -0500

    Add files via upload

---
+++ procesador-4/isim/tswinMan_isim_beh.exe.sim/work/a_3915426006_2372691052.c
@@ -0,0 +1,147 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/Martinez/Downloads/procesador4/tswinMan.vhd";
+
+
+
+static void work_a_3915426006_2372691052_p_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    int64 t7;
+    char *t8;
+    char *t9;
+
+LAB0:    t1 = (t0 + 3792U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(100, ng0);
+    t2 = (t0 + 4176);
+    t3 = (t2 + 56U);
+    t4 = *((char **)t3);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    *((unsigned char *)t6) = (unsigned char)3;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(101, ng0);
+    t2 = (t0 + 4240);
+    t3 = (t2 + 56U);
+    t4 = *((char **)t3);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    *((unsigned char *)t6) = (unsigned char)2;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(102, ng0);
+    t7 = (100 * 1000LL);
+    t2 = (t0 + 3600);
+    xsi_process_wait(t2, t7);
+
+LAB6:    *((char **)t1) = &&LAB7;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(103, ng0);
+    t2 = (t0 + 4176);
+    t3 = (t2 + 56U);
+    t4 = *((char **)t3);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    *((unsigned char *)t6) = (unsigned char)2;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(104, ng0);
+    t2 = (t0 + 7004);
+    t4 = (t0 + 4304);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t8 = (t6 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 5U);
+    xsi_driver_first_trans_fast(t4);
+    xsi_set_current_line(105, ng0);
+    t2 = (t0 + 7009);
+    t4 = (t0 + 4368);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t8 = (t6 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 5U);
+    xsi_driver_first_trans_fast(t4);
+    xsi_set_current_line(106, ng0);
+    t2 = (t0 + 7014);
+    t4 = (t0 + 4432);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t8 = (t6 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 5U);
+    xsi_driver_first_trans_fast(t4);
+    xsi_set_current_line(107, ng0);
+    t2 = (t0 + 7019);
+    t4 = (t0 + 4496);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t8 = (t6 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 2U);
+    xsi_driver_first_trans_fast(t4);
+    xsi_set_current_line(108, ng0);
+    t2 = (t0 + 7021);
+    t4 = (t0 + 4560);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t8 = (t6 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 6U);
+    xsi_driver_first_trans_fast(t4);
+    xsi_set_current_line(117, ng0);
+
+LAB10:    *((char **)t1) = &&LAB11;
+    goto LAB1;
+
+LAB5:    goto LAB4;
+
+LAB7:    goto LAB5;
+
+LAB8:    goto LAB2;
+
+LAB9:    goto LAB8;
+
+LAB11:    goto LAB9;
+
+}
+
+
+extern void work_a_3915426006_2372691052_init()
+{
+	static char *pe[] = {(void *)work_a_3915426006_2372691052_p_0};
+	xsi_register_didat("work_a_3915426006_2372691052", "isim/tswinMan_isim_beh.exe.sim/work/a_3915426006_2372691052.didat");
+	xsi_register_executes(pe);
+}
